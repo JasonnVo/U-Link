df5ac59a72cd4e63178c27f62261366cbaf812f4
DongpeiZ <david.zhang1459@gmail.com>
2025-03-13 16:48:19 -0400

    created the look of the login page

---
+++ src/Components/Profile.tsx
@@ -0,0 +1,13 @@
+import './Profile.css'
+
+function Profile() {
+    return (
+        <>
+            <div className="profile">
+                <h1>Profile Page</h1>
+            </div>
+        </>
+    );    
+}
+
+export default Profile;
