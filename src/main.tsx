049a3d7c702de0942edd3288cca2382f2f0d023d
SecureDAO <93850661+SecureDAO@users.noreply.github.com>
2025-03-14 15:07:40 -0400

    Should be updated with main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './main.css';
-import { Leaderboard } from './Components/Leaderboard';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./main.css";
 // component imports
-import HeaderContainer from './Components/HeaderContainer';
-import BusTracker from './Components/BusTracker';
-import NavBar from './Components/NavBar';  
-import Map from './Components/Map'; 
-import Login from './Components/Login';
-import Profile from './Components/Profile';
+import HeaderContainer from "./Components/HeaderContainer";
+import BusTracker from "./Components/BusTracker";
+import NavBar from "./Components/NavBar";
+import Map from "./Components/Map";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+import Leaderboard from "./Components/Leaderboard";
 import BusRoutesList from './BusRoutesList';
 
 createRoot(document.getElementById('root')!).render(
