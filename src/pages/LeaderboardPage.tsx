2059d141b9b620bf741104292604df8702c01114
SecureDAO <93850661+SecureDAO@users.noreply.github.com>
2025-03-21 03:07:52 -0400

    Added a leaderboard that automatically puts the rider with the most points first (arbitrary for now but can be equivalent to rides * 10, or minutes traveled, or miles, etc.)

--- src/pages/LeaderboardPage.tsx
+++ src/pages/LeaderboardPage.tsx
@@ -2,7 +2,10 @@ import Leaderboard from "../Components/Leaderboard";
 
 function LeaderboardPage() {
 	return (
-		<div>
+		<div className="my-20 mx-auto max-w-4xl p-6">
+			<h1 className="text-4xl font-bold text-center mb-8 text-white drop-shadow-lg">
+				Student Leaderboard
+			</h1>
 			<Leaderboard />
 		</div>
 	);
