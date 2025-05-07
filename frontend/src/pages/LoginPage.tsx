487696122029e53b7752caa08faf7021691766b0
Jason Vo <jason54423@gmail.com>
2025-05-07 02:17:23 -0400

    adjusted login and profile heights slightly

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import Login from "../Components/Login";
 function LoginPage() {
 	return (
 		<div className="w-full min-h-screen p-4">
-			<h1 className="mt-20 text-4xl font-bold text-center mb-8 text-white drop-shadow-lg">
+			<h1 className="mt-50 text-4xl font-bold text-center mb-8 text-white drop-shadow-lg">
 				Login Page
 			</h1>
 			<Login />
